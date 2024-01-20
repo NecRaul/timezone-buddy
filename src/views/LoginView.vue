39aa15ee6aea452f3078a91b5fe3941d14858608
NecRaul <necraul@kuroneko.dev>
2024-01-20 07:33:26 +0400

    Login View

---
+++ src/views/LoginView.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { ref } from 'vue'
+import {
+  getAuth,
+  signInAnonymously,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signInWithEmailAndPassword
+} from 'firebase/auth'
+
+const auth = getAuth()
+const email = ref('')
+const password = ref('')
+const msg = ref()
+
+async function authenticate(provider) {
+  msg.value = ''
+  try {
+    switch (provider) {
+      case 'email':
+        await signInWithEmailAndPassword(auth, email.value, password.value)
+        break
+      case 'anonymous':
+        await signInAnonymously(auth)
+        break
+      case 'google':
+        await signInWithPopup(auth, new GoogleAuthProvider())
+        break
+      case 'register':
+        // route to register
+        return
+      case 'reset':
+        // route to reset password
+        return
+    }
+    // route to buddies
+  } catch (error) {
+    msg.value = error.message
+  }
+}
+</script>
+
+<template>
+  <div class="creds">
+    <p><input type="text" placeholder="Email" v-model="email" /></p>
+    <p><input type="password" placeholder="Password" v-model="password" /></p>
+    <p v-if="msg">{{ msg }}</p>
+    <p><button @click="authenticate('email')">Sign In With Email</button></p>
+    <p><button @click="authenticate('google')">Sign In With Google</button></p>
+    <p><button @click="authenticate('anonymous')">Sign In Anonymously</button></p>
+    <p><button @click="authenticate('register')">Register</button></p>
+    <p><button @click="authenticate('reset')">Forgot Password?</button></p>
+  </div>
+</template>
